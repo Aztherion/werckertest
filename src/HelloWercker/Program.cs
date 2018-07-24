7b5a9041dea9493f55f38e7196bff1ac96b1460d
Benny Olsson <benny.olsson@infostep.se>
2018-07-24 10:27:50 +0200

    aspnet

--- src/HelloWercker/Program.cs
+++ src/HelloWercker/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 
 namespace HelloWercker
 {
@@ -6,7 +10,21 @@ namespace HelloWercker
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var config = new ConfigurationBuilder().AddCommandLine(args).Build();
+
+            var host = new WebHostBuilder().UseKestrel().UseStartup<Startup>().UseConfiguration(config).Build();
+
+            host.Run();
+        }
+    }
+
+    class Startup
+    {
+        public Startup(IHostingEnvironment env) { }
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            app.Run(async (context) => { await context.Response.WriteAsync("Hello!"); });
         }
     }
 }
